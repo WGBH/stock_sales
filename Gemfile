9da72cc605a0a88ffeba9f924ca34d01e4cee070
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-10-23 16:26:14 -0400

    Adds devise gem to the project

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+gem 'devise'
 gem 'bootstrap-sass'
 gem 'compass-rails', '~> 1.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get 'styleguide' => 'styleguide#index'
 
+  #  TODO
+  #  root to: "something"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
