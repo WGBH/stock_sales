5b0e87ccb676875226165f15c522ffcc2025ff60
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-10-29 14:02:33 -0400

    Adds validation for user fields. Changes default user registration page to include all fields from current site. Fixes #16.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_uniqueness_of :email, message: "must be unique"
+  validates_presence_of :password_confirmation, message: "must be same as password"
+  validates_presence_of :first_name, message: "can't be blank"
+  validates_presence_of :last_name, message: "can't be blank"
+  validates_presence_of :phone, message: "can't be blank"
+  validates :phone, length: { is: 10, wrong_length: "must be %{count} digits" }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
