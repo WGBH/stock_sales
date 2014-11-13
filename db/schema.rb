e94bc2b0f7fd79091412c5222c4158752f8da04f
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-11-13 15:18:08 -0500

    Adds HeroImage model. Fixes #63.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112203049) do
+ActiveRecord::Schema.define(version: 20141113193300) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 20141112203049) do
   end
 
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
+
+  create_table "hero_images", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "searches", force: true do |t|
     t.text     "query_params"
