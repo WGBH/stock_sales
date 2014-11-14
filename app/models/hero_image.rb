5ece9c57319b8e6f52b6e53937c002691c8c7fbd
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-11-14 12:25:19 -0500

    Adds validation and tests for the HeroImage model.

--- app/models/hero_image.rb
+++ app/models/hero_image.rb
@@ -1,2 +1,7 @@
 class HeroImage < ActiveRecord::Base
+  attr_accessor :title, :alt, :image
+
+  validates_presence_of :title
+  validates_presence_of :alt
+  validates_presence_of :image
 end
--- spec/models/hero_image_spec.rb
+++ spec/models/hero_image_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe HeroImage, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe HeroImage do
+
+  describe '.new' do
+    it 'is not valid without title' do
+      heroimg = HeroImage.new(alt: 'alt', image: 'image')
+      expect(heroimg).to_not be_valid
+    end
+    
+    it 'is not valid without alt' do
+      heroimg = HeroImage.new(title: 'title', image: 'image')
+      expect(heroimg).to_not be_valid
+    end
+
+    it 'is not valid without image' do
+      heroimg = HeroImage.new(title: 'title', alt: 'alt')
+      expect(heroimg).to_not be_valid
+    end
+
+    it 'is valid with title, alt, and image' do
+      heroimg = HeroImage.new(title: 'title', alt: 'alt', image: 'image')
+      expect(heroimg).to be_valid
+    end
+  end
 end
