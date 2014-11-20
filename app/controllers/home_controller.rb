c4f8ec62a44b98666ae4bb5e10b864f55c5e8381
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-11-20 13:02:32 -0500

    Adds four sidekick images to the home page. Fixes #68.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
   def home 
     @hero_img = HeroImage.first
     @hero_url = @hero_img.image.url 
+    @sidekick_imgs = []
+    (1..4).each do |n|
+      @sidekick_imgs << (SidekickImage.find_by order: n)
+    end
   end
 end
--- app/uploaders/sidekick_image_uploader.rb
+++ app/uploaders/sidekick_image_uploader.rb
@@ -4,7 +4,7 @@ class SidekickImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -35,6 +35,9 @@ class SidekickImageUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
+
+  # Resizes uploaded images for fitting on the home page
+  process resize_to_fit: [500, 500]
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
