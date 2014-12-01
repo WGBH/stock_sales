408070d40a64c7bce9311532cef0199f02c3ff9a
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-12-01 09:35:58 -0500

    Centers a blacklight search bar on top of the hero image. Fixes #70.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(partials/_user_layout.css)
+Rails.application.config.assets.precompile += %w(partials/_search_form.css)
 
